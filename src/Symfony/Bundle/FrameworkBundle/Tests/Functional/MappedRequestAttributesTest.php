e86d91bb8e5e53a326d3ef4e36ea4ee0e5c08f37
Konstantin Myakshin <molodchick@gmail.com>
2023-01-28 22:36:16 +0200

    Add generic exception handler for ValidationFailedException

--- src/Symfony/Bundle/FrameworkBundle/Tests/Functional/MappedRequestAttributesTest.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/Functional/MappedRequestAttributesTest.php
@@ -11,22 +11,28 @@
 
 namespace Symfony\Bundle\FrameworkBundle\Tests\Functional;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryString;
 use Symfony\Component\HttpKernel\Attribute\MapRequestContent;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class MappedRequestAttributesTest extends AbstractWebTestCase
 {
     public function testMapQueryString()
     {
+        //todo: add data provider, test validation?
         $client = self::createClient(['test_case' => 'MappedRequestAttributes']);
 
         $client->request('GET', '/map-query-string', ['filter' => ['status' => 'approved', 'quantity' => 4]]);
 
-        self::assertEquals('filter.status=approved,filter.quantity=4', $client->getResponse()->getContent());
+        self::assertSame('filter.status=approved,filter.quantity=4', $client->getResponse()->getContent());
     }
 
-    public function testMapRequestContent()
+    /**
+     * @dataProvider mapRequestContentProvider
+     */
+    public function testMapRequestContent(string $content, string $expectedResponse, int $expectedStatusCode)
     {
         $client = self::createClient(['test_case' => 'MappedRequestAttributes']);
 
@@ -36,14 +42,63 @@ class MappedRequestAttributesTest extends AbstractWebTestCase
             [],
             [],
             [],
-            <<<'JSON'
+            $content
+        );
+
+        self::assertJsonStringEqualsJsonString($expectedResponse, $client->getResponse()->getContent());
+        self::assertSame($expectedStatusCode, $client->getResponse()->getStatusCode());
+    }
+
+    public static function mapRequestContentProvider(): iterable
+    {
+        yield 'valid json' => [
+            'content' => <<<'JSON'
 {
     "comment": "Hello everyone!"
 }
-JSON
-        );
+JSON,
+            'expectedResponse' => <<<'JSON'
+{
+    "comment": "Hello everyone!"
+}
+JSON,
+            'expectedStatusCode' => 200,
+        ];
 
-        self::assertEquals('comment=Hello everyone!', $client->getResponse()->getContent());
+        yield 'invalid json' => [
+            'content' => <<<'JSON'
+{
+    "comment": ""
+}
+JSON,
+            'expectedResponse' => <<<'JSON'
+{
+    "type": "https:\/\/symfony.com\/errors\/validation",
+    "title": "Validation Failed",
+    "detail": "comment: This value should not be blank.\ncomment: This value is too short. It should have 10 characters or more.",
+    "violations": [
+        {
+            "propertyPath": "comment",
+            "title": "This value should not be blank.",
+            "parameters": {
+                "{{ value }}": "\"\""
+            },
+            "type": "urn:uuid:c1051bb4-d103-4f74-8988-acbcafc7fdc3"
+        },
+        {
+            "propertyPath": "comment",
+            "title": "This value is too short. It should have 10 characters or more.",
+            "parameters": {
+                "{{ value }}": "\"\"",
+                "{{ limit }}": "10"
+            },
+            "type": "urn:uuid:9ff3fdc4-b214-49db-8718-39c315e33d45"
+        }
+    ]
+}
+JSON,
+            'expectedStatusCode' => 400,
+        ];
     }
 }
 
@@ -59,7 +114,7 @@ class WithMapRequestContentController
 {
     public function __invoke(#[MapRequestContent] RequestContent $content): Response
     {
-        return new Response("comment={$content->comment}");
+        return new JsonResponse(['comment' => $content->comment]);
     }
 }
 
@@ -80,7 +135,10 @@ class Filter
 
 class RequestContent
 {
-    public function __construct(public readonly string $comment)
-    {
+    public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Length(min: 10)]
+        public readonly string $comment
+    ) {
     }
 }
